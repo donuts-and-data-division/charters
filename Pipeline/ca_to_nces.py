91677b7a15747a8d6f4be887c9e5bf89edc5e6d5
joan-wang <joanyiqunwang@gmail.com>
2017-05-19 23:20:35 -0500

    codes mapping

---
+++ Pipeline/ca_to_nces.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+nces = pd.read_csv("../Data/NCES_full.csv", dtype={'School ID - NCES Assigned [Public School] Latest available year': 'str'})
+ca = pd.read_csv("../Data/ca_pubschls.csv", encoding='ISO-8859-1', dtype={"CDSCode": 'str', "NCESCode": 'str', "NCESDist": 'str', "NCESSchool": 'str'})
+
+# concat nces cols to from ca
+nces['School ID - NCES Assigned [Public School] Latest available year'] = '0' + nces['School ID - NCES Assigned [Public School] Latest available year']
+master = pd.merge(ca[ca.Charter == 'Y'], nces, how='inner', left_on='NCESCode', right_on='School ID - NCES Assigned [Public School] Latest available year', suffixes=('_ca','_nces'))
