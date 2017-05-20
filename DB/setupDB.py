b8a387bc9df5d81c0e3d62077d97dffd06a6bfbe
Ari <anisfeld@uchicago.edu>
2017-05-19 21:01:03 -0500

    small change

--- DB/setupDB.py
+++ DB/setupDB.py
@@ -5,21 +5,21 @@ from os import path, system
 
 
 @click.command()
-@click.option('--db', help='YAML-formatted database connection credentials.', required=True)
+@click.option('--yml', help='YAML-formatted database connection credentials.', required=True)
 @click.option('--ddl', help='sql file.', required=True)
 @click.option('--csv', help='CSV file to load into the database table', required=True)
-def main(db, ddl, csv):
-    init(db, ddl, csv)
+def main(yml, ddl, csv):
+    init(yml, ddl, csv)
 
-def init(db, ddl, csv):
+def init(yml, ddl, csv):
     # We'll shell out to `psql`, so set the environment variables for it:
-    with open(db) as f:
-        for k,v in yaml.load(f).items():
-            os.environ['PG' + k.upper()] = str(v) if v else ""
+    #with open(yml) as f:
+    #    for k,v in yaml.load(f).items():
+    #        os.environ['PG' + k.upper()] = str(v) if v else ""
 
     # And create the table from the csv file with psql
-    system(""" psql -h 127.0.0.1 -f {} testdb -U cappuser """.format(ddl))
-    system("""sudo service postgresql restart""")
+    system("""psql -h pg.rcc.uchicago.edu -f {} capp30254_project1 -U capp30254_project1_user""".format(ddl))
+    #system("""sudo service postgresql restart""")
     system("""psql -c "\copy {} FROM '{}' WITH CSV HEADER;" """.format("schools", csv))
     
     #system("""psql -c "alter table dedupe.entries add column entry_id SERIAL PRIMARY KEY;" """)
@@ -28,3 +28,7 @@ def init(db, ddl, csv):
 
 if __name__ == '__main__':
     main()
+
+    #df.to_csv("test.csv")
+
+   # system("""csvsql --db “postgresql://capp30254_project1_user:bokMatofAtt.@pg.rcc.uchicago.edu:5432/capp30254_project1”  --insert {} --overwrite""".format(csv))
