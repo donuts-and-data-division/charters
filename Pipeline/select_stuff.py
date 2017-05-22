76c2c0e7e79334cec22a319ffbed8cbfad4a4070
joan-wang <joanyiqunwang@gmail.com>
2017-05-22 15:30:02 -0500

    select update

--- Pipeline/select_stuff.py
+++ Pipeline/select_stuff.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
-import sys
 import pandas as pd
 import numpy as np
-from config import *
-#from cleaning import *
-#from model import *
-#from features import *
-from explore import *
-from sklearn.cross_validation import train_test_split
+from db_config import *
+
 
 def select_statement():
     #need to rename "pupil/teacher_ratio_public_school_2012-13" column in order to select it i dont think it likes that slash
@@ -43,19 +38,4 @@ def select_statement():
     return df
 
 
-def pipeline(df):
-    explore(df)
-    '''
-    df = clean(df)
-    X_train, X_test, y_train, y_test = train_test_split(df[FEATURE_COLS], df[OUTCOME_VAR], test_size=TEST_SIZE, random_state=0)
-    X_train = feature_eng(X_train)
-    X_test = feature_eng(X_test)
-    results = classifiers_loop(X_train, X_test, y_train, y_test)
-    results.to_csv('results.csv')
-    '''
-    return results, y_test
 
-if __name__=="__main__":
-    
-    df = select_statement()
-    results, y_test = pipeline(df)
