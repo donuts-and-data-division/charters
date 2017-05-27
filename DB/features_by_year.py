82f3c4278f5c8064bb12fe4dd72bef1b3d7b4f21
techbar <joanyiqunwang@gmail.com>
2017-05-27 00:23:23 -0500

    financials function working

--- DB/features_by_year.py
+++ DB/features_by_year.py
@@ -25,14 +25,29 @@ def edit_financials(year):
 		year: (str) representing the suffix of financial table name, e.g. "11"
 	'''
 
+	# update data types and create CDSCode column
+	alteration = """
+		ALTER TABLE "Alternate_Form_Data_{year}"
+			ALTER COLUMN restricted TYPE NUMERIC USING (restricted::numeric),
+			ALTER COLUMN unrestricted TYPE NUMERIC USING (unrestricted::numeric),
+			ALTER COLUMN total TYPE NUMERIC USING (total::numeric);
+		
+		ALTER TABLE "Alternate_Form_Data_{year}"
+			ADD COLUMN "CDSCode" VARCHAR(20);
+
+		UPDATE "Alternate_Form_Data_{year}"
+		SET "CDSCode" = concat("Ccode","Dcode","SchoolID");
+		""".format(year=year)
+	db_action(alteration, action_type='alter')
+
 	# get initial df
-	query = """
+	select = """
 		SELECT "CDSCode", "ObjectCode", "Description", total
 		FROM "Alternate_Form_Data_{year}" 
 		JOIN "Objects_full" 
 		ON "Alternate_Form_Data_{year}"."ObjectCode" = "Objects_full"."Object" 
 	    ;""".format(year=year)
-	df = db_selection(query)
+	df = db_action(select, action_type='select')
 
 	# modify df
 	modified_df = pd.pivot_table(df, values='total', index='CDSCode', columns=['ObjectCode'], aggfunc=np.sum)
@@ -41,42 +56,25 @@ def edit_financials(year):
 	modified_df.to_csv('../Data/financials_{year}_wide.csv'.format(year=year))
 	
 	# load csv to database
-	system_str = '''csvsql --db "postgresql://{}:{}@{}:{}/{}"  --insert ~/charters/Data/financials_{}_wide.csv --no-inference'''.format(USER, PASSWORD, HOST, PORT, DATABASE, year)
+	system_str = '''csvsql --db "postgresql://{}:{}@{}:{}/{}"  --insert ~/charters/Data/financials_{}_wide.csv'''.format(USER, PASSWORD, HOST, PORT, DATABASE, year)
 	try:
 		system(system_str)
 	except:
 		print('did not create table!')
 
-def db_selection(query):
+def db_action(query, action_type):
 	'''
 	Utility function to run SQL query and return results in a dataframe
 	'''
 	db_string = 'postgresql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, HOST, PORT, DATABASE)
 	engine = create_engine(db_string)
-	df = pd.read_sql_query(query, engine)
-	return df
-
-def create_table(table_group, year, schema_specifics):
-	'''
-	Insert completed table (wide version) into db
-
-	Inputs
-		table_group: (str) "financial", "cohort", etc
-		year: (str) "11", "12", etc
-		schema_specifics: (str) table creation specifications
-
-	Outputs
-		None. Inserts new table into db.
-	'''
-
-	query = '''
-		DROP TABLE IF EXISTS {table_group}_{year}; 
-		CREATE TABLE {table_group}_{year} 
-		({schema_specifics}); 
-		COPY {table_group}_{year} FROM '../Data/{table_group}_{year}.csv' DELIMITER ',' CSV;
-		'''.format(table_group=table_group, year=year, schema_specifics=schema_specifics)
-
-	db_selection(query)
+	if action_type == 'select': 
+		df = pd.read_sql_query(query, engine)
+		return df
+	elif action_type == 'alter':
+		system("psql -d capp30254_project1 -U capp30254_project1_user -h pg.rcc.uchicago.edu -c '{}'".format(query))
+	else:
+		print('invalid action type')
 	
 
 if __name__=="__main__":
