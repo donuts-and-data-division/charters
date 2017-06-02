4551ede6dc05c4ba0273b829dc0d074a45dbd719
Ari <anisfeld@uchicago.edu>
2017-06-02 11:14:26 -0500

    sublime

--- Scripts/BST.sh
+++ Scripts/BST.sh
@@ -11,4 +11,4 @@
 module load python/3.5.2+intel-16.0
 cd /home/anisfeld/scratch-midway/ml-charters
 source /home/anisfeld/scratch-midway/ml-charters/bin/activate
-python /home/anisfeld/scratch-midway/ml-charters/charters/Pipeline/pipeline_test.py BEST_GRID RF out_best_1.csv python /home/anisfeld/scratch-midway/ml-charters/charters/Pipeline/pipeline_test.py SMALL_GRID DT out_DT_1.csv ~/home/anisfeld/scratch-midway/ml-charters/charters/Pipeline/queryresults.csv
+python /home/anisfeld/scratch-midway/ml-charters/charters/Pipeline/pipeline_test.py BEST_GRID RF out_best_1.csv /home/anisfeld/scratch-midway/ml-charters/charters/Pipeline/queryresults.csv
